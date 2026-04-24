6af8ffee5db62a01e0acb3c3565bb9a13c4d32d7
Giovana-Manuquian <gmanuquian@gmail.com>
2026-04-24 10:39:09 -0300

    config register page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -20,7 +20,7 @@ export default function RegisterPage() {
     setLoading(true)
 
     try {
-      const response = await fetch('http://localhost:8080/users', {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`,{
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
